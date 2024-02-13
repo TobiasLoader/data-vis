5cc4f897265b192c3456beefca46829f1bf138d7
Toby <toby@tobias.codes>
2024-02-13 16:54:43 +0000

    changes to helper funcs

--- assets/js/helper.js
+++ assets/js/helper.js
@@ -1,9 +1,19 @@
-function appendChart(id,svg,title,description) {
-  d3.select("#chart-container").append('div')
+function appendChart(id,svg,title,description,labelfuncs=[]) {
+  let existing_element = d3.select("#chart-container #"+id);
+  
+  if (existing_element.empty()) {
+    d3.select("#chart-container").append('div')
     .attr('id', id)
     .attr('class', 'chart');
+  }
+  
   d3.select("#"+id).append('div').attr('class', 'chart-label');
   d3.select("#"+id + " .chart-label").append("h3").text(title);
   d3.select("#"+id + " .chart-label").append("p").text(description);
+  if (labelfuncs.length>0){
+    d3.select("#"+id + " .chart-label").append('div').attr('class','menu-custom');
+    for (let f of labelfuncs) f(d3.select("#"+id + " .menu-custom"));
+  }
+  
   d3.select("#"+id).append(() => svg.node());
 }
